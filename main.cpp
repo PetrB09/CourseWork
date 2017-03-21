5108129248f4753cc994e9c594412625bd4fe988
PetrB09 <petrb1996@gmail.com>
2017-03-21 14:37:37 +0300

    adding log out after loading image

--- main.cpp
+++ main.cpp
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <vector_types.h>
 
 #include <cv.h>
-//#include <HandlingImgOnGPU.h>
+#include <HandlingImgOnGPU.h>
 #include <opencv2/cudaimgproc.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <cuda.h>
-#include <cuda_runtime.h>
+#include <vector_types.h>
+#include <opencv2/dnn.hpp>
+
 
 using namespace std;
 using namespace cv;
 using namespace cuda;
+//using namespace cv::dnn
 
 int main(int argc, char **argv)
 {
@@ -22,12 +24,26 @@ int main(int argc, char **argv)
 	string inputPathR = "inputR.png";
 	string outputPathL = "outputL.png";
 	string outputPathR = "outputR.png";
+	char* logPath = (char*)"tmp.log";
+	ofstream logOut;
+	logOut.open(logPath);
 
-	cv::Mat LeftPic = imread(inputPathL, -1);//reading image from folder
-	cv::Mat RightPic = imread(inputPathR, -1);
+	logOut<< "reading files " << inputPathL << " " << inputPathR;
 
+	cv::Mat LeftPic = cv::imread(inputPathL, CV_LOAD_IMAGE_COLOR);//reading image from folder
+	cv::Mat RightPic = cv::imread(inputPathR, CV_LOAD_IMAGE_COLOR);
+	if(LeftPic.empty() || RightPic.empty())
+		logOut << "Files not readed\n";
+	else
+		logOut << "Files succesfuly readed\n";
+	uint Stime = clock();
 	cv::cuda::GpuMat GPULeft(LeftPic); //loading inage to GPU
 	cv::cuda::GpuMat GPURight(RightPic);
+
+	logOut << "size of image " << GPULeft.size() << "\n";
+	logOut << "type of element " << GPULeft.type() << "\n";
+	logOut << "size of element " << GPULeft.elemSize() << "\n";
+	logOut << "size element on channel " << GPULeft.elemSize1() << "\n";
 
 	cuda::bilateralFilter(GPULeft, GPULeft, 5, 150, 150); //Handling image
 	cuda::bilateralFilter(GPURight, GPURight, 5, 150, 150);
@@ -35,8 +51,19 @@ int main(int argc, char **argv)
 	GPULeft.download(LeftPic); //return image to CPU
 	GPURight.download(RightPic);
 
-	imwrite(outputPathL, LeftPic, vector<int>(-1)); //save image
-	imwrite(outputPathR, RightPic, vector<int>(-1));
+	uint Ftime = clock();
 
+	bool LeftWrited = imwrite(outputPathL, LeftPic, vector<int>(CV_IMWRITE_PNG_COMPRESSION, 0)); //save image
+	bool RightWrited = imwrite(outputPathR, RightPic, vector<int>(CV_IMWRITE_PNG_COMPRESSION, 0));
+
+	vector<uchar4***> pointers;
+
+
+	if(!LeftWrited && !RightWrited)
+		logOut << "Error of writing\n";
+	else
+		logOut << "Images succesfuly writed\n";
+	logOut << "runtime: "<< (float)(Ftime - Stime)/CLOCKS_PER_SEC;
+	logOut.close();
 	return 0;
 }
